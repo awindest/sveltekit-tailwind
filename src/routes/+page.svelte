e3cf1dedb6979517f631ff9fdca3e33a2db991b0
awindest <william.indest@gmail.com>
2024-08-14 05:49:01 -0400

    Indest Labs icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-<h1 class="text-3xl bold-font underline"> Tailwind Experiment using Sveltekit</h1>
+
+<div class="w-screen h-screen bg-black">
+	<div class="absolute top-0 w-full pointer-events-none ">
+		<h1 class="text-center my-6 text-white w-full font-bold text-6xl">Tailwind Experiment using Sveltekit</h1>
+	</div>
+</div>
